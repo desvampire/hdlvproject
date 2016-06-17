422a4521e410fa7ca2e5658f100a7540a593c03d
desvampire <io8080115@gmail.com>
2016-06-17 16:51:56 +0300

    added lessons 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,6 @@
 /*
  * Custom
  */
+
 //= partials/dropdown-plugin.js
-//= partials/social-likes.js
-//= partials/simple-tabs.js
 //= partials/app.js
-//= partials/cabinet.js
-//= partials/navbars.js
